832c5e4c4ebeed5047b25d673e63eb43f3f56d3b
ricky-blip <ricky.rinaldy77@gmail.com>
2022-04-26 15:11:52 +0700

    Tugas 2 Javascript

---
+++ tugas2.js
@@ -0,0 +1,20 @@
+var tipedataNumber  = 1;
+var tipedataString  = "Ricky Rinaldy";
+var tipedataBoolean = true;
+var tipedataNull    = null;
+var tipedataArray   = [1,2,3,4,5,"Ini adalah tipe data array"];
+
+console.log(tipedataNumber);
+console.log(tipedataString);
+console.log(tipedataBoolean);
+console.log(tipedataNull);
+console.log(tipedataArray);
+
+console.log("\n"+
+    " Tipe Data Number = " + tipedataNumber, "\n",
+    "Tipe Data String = " + tipedataString, "\n",
+    "Tipe Data Boolean = " + tipedataBoolean, "\n",
+    "Tipe Data Null = " + tipedataNull, "\n",
+    "Tipe Data Array = " + tipedataArray
+)
+
